d58b4406b31a7cc5b2f53804265784b8a653a11e
Anh Khoi <anhkhoi5602@gmail.com>
2025-12-11 00:45:00 +0700

    Test

--- src/algorithms/euler.py
+++ src/algorithms/euler.py
@@ -2,35 +2,13 @@ from models.graph import Graph
 
 class Euler():
     @staticmethod
-    def fleury(graph, start):
-    # dfs
-        def dfs(u, visited):
-            visited.add(u)
-            for v in graph[u]:
-                if v not in visited:
-                    dfs(v, visited)
+    def fleury(graph:Graph, start):
+        edges = graph.get_edges()
 
         # kiểm tra cạnh có phải cầu không
         def is_not_bridge(u, v):
-            if len(graph[u]) == 1:
-                return True
-
-            visited = set()
-            dfs(u, visited)
-            count1 = len(visited)
-
-            graph[u].remove(v)
-            graph[v].remove(u)
-
-            visited = set()
-            dfs(u, visited)
-            count2 = len(visited)
-
-            graph[u].append(v)
-            graph[v].append(u)
-
-            return count1 == count2
-
+            while len(u) == 1:
+                
         # kiểm tra điều kiện Euler
         def has_euler():
             odd = 0
@@ -39,18 +17,18 @@ class Euler():
                     odd += 1
             return odd == 0 or odd == 2
 
-        # tìm điểm bắt đầu (nếu không truyền start), trường hợp là đường đi Euler thì sẽ bắt đầu tại đỉnh lẻ
+        # tìm điểm bắt đầu (nếu không truyền start)
         def find_start():
             for u in graph:
                 if len(graph[u]) % 2 == 1:
                     return u
             return list(graph.keys())[0]
 
-        # xử lý start nếu không set đúng
+        # xử lý start nếu người dùng không set đúng
         if start is None:
             start = find_start()
 
-        # nếu không có đường đi Euler thì dừng
+        # nếu không có đường đi Euler → dừng
         if not has_euler():
             return None
 
